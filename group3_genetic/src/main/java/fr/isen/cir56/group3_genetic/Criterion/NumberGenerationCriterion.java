87c6c29c65ce49dbfb4a0736e33a89bfb6bd31f8
Louis VICAINNE <louis@vicainne.fr>
2013-03-09 12:13:15 +0000

    Refactoring de Criterion en Constraint Améliorationde l'aspect FitnessFunction & Chromosome

--- group3_genetic/src/main/java/fr/isen/cir56/group3_genetic/Criterion/NumberGenerationCriterion.java
+++
@@ -1,6 +0,0 @@
-package fr.isen.cir56.group3_genetic.Criterion;
-		
-		
-public abstract class NumberGenerationCriterion implements StopCriterion {
-	
-}
--- group3_genetic/src/main/java/fr/isen/cir56/group3_genetic/Criterion/TimeElapsedCriterion.java
+++
@@ -1,5 +0,0 @@
-package fr.isen.cir56.group3_genetic.Criterion;
-
-public abstract class TimeElapsedCriterion implements StopCriterion {
-	
-}
