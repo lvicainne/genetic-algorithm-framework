9a25d0acd77aabf95bac74e54f34395a568cfcae
Louis VICAINNE <louis@vicainne.fr>
2013-03-09 15:57:43 +0000

    Modification de Configuration : Ajout de tous les paramètres nécessaires. Vérification de la validité de la configuration avant un verrouillage pour lancer le calcul

--- group3_genetic/src/main/java/fr/isen/cir56/group3_genetic/Configuration/Configuration.java
+++ group3_genetic/src/main/java/fr/isen/cir56/group3_genetic/Configuration/Configuration.java
@@ -1,17 +1,164 @@
 package fr.isen.cir56.group3_genetic.Configuration;
 
-import fr.isen.cir56.group3_genetic.Constraint.Constraint;
-import fr.isen.cir56.group3_genetic.Selector.Selector;
+import fr.isen.cir56.group3_genetic.AbstractFitnessFunction;
+import fr.isen.cir56.group3_genetic.Breeder.BreederInterface;
+import fr.isen.cir56.group3_genetic.Constraint.ConstraintInterface;
+import fr.isen.cir56.group3_genetic.Genotype.ChromosomeFactoryInterface;
+import fr.isen.cir56.group3_genetic.Operator.OperatorInterface;
+import fr.isen.cir56.group3_genetic.Selector.SelectorInterface;
+import java.util.LinkedList;
+import java.util.List;
 
-public class Configuration extends AbstractConfiguration {
 
-	public Configuration(Selector selector, Constraint stopCriterion, int initialSizePopulation, int sizeSelection) {
-		super(selector, stopCriterion, initialSizePopulation, sizeSelection);
+public class Configuration implements ConfigurationInterface {
 
+	private List<ConstraintInterface> constraints;
+	private List<SelectorInterface> selectors;
+	private List<OperatorInterface> operators;
+	private AbstractFitnessFunction fitnessFunction;
+	private BreederInterface breeder;
+	private ChromosomeFactoryInterface chromosomeFactory;
+	private int populationSize;
+	private boolean isLocked;
 
+	public Configuration() {
+		this.constraints = new LinkedList<ConstraintInterface>();
+		this.selectors = new LinkedList<SelectorInterface>();
+		this.operators = new LinkedList<OperatorInterface>();
 	}
 	
 	
+	public void addConstraint(ConstraintInterface Constraint) throws InvalidConfigurationException {
+		this.checkBeforeEditSettings();
+		
+		if(Constraint == null) {
+			throw new NullPointerException();
+		}
+		this.constraints.add(Constraint);
+	}
+
+	public void removeConstraint(ConstraintInterface Constraint) throws InvalidConfigurationException {
+		this.checkBeforeEditSettings();
+		this.removeConstraint(Constraint);
+	}
+
+	public List<ConstraintInterface> getConstraints() {
+		return this.constraints;
+	}
 	
+	public void addSelector(SelectorInterface selector) throws InvalidConfigurationException {
+		this.checkBeforeEditSettings();
+		if(selector == null) {
+			throw new NullPointerException();
+		}
+		this.selectors.add(selector);
+	}
+
+	public void removeSelector(SelectorInterface selector) throws InvalidConfigurationException {
+		this.checkBeforeEditSettings();
+		this.removeSelector(selector);
+	}
+
+	public List<SelectorInterface> getSelectors() {
+		return this.selectors;
+	}
+
+	public void addOperator(OperatorInterface operator) throws InvalidConfigurationException {
+		this.checkBeforeEditSettings();
+		if(operator == null) {
+			throw new NullPointerException();
+		}
+		this.operators.add(operator);
+	}
+
+	public void removeOperator(OperatorInterface operator) throws InvalidConfigurationException {
+		this.checkBeforeEditSettings();
+		this.removeOperator(operator);
+	}
+
+	public List<OperatorInterface> getOperators() {
+		return this.operators;
+	}
+
+	public AbstractFitnessFunction getFitnessFunction() {
+		return this.fitnessFunction;
+	}
+
+	public void setFitnessFunction(AbstractFitnessFunction fitnessFunction) throws InvalidConfigurationException {
+		this.checkBeforeEditSettings();
+		this.fitnessFunction = fitnessFunction;
+	}
+
+	public void setBreeder(BreederInterface breeder) throws InvalidConfigurationException {
+		this.checkBeforeEditSettings();
+		this.breeder = breeder;
+	}
+
+	public BreederInterface getBreeder() {
+		return this.breeder;
+	}
+
+	public void setChromosomeFactory(ChromosomeFactoryInterface chromosomeFactory) throws InvalidConfigurationException {
+		this.checkBeforeEditSettings();
+		this.chromosomeFactory = chromosomeFactory;
+	}
+
+	public ChromosomeFactoryInterface getChromosomeFactory() {
+		return this.chromosomeFactory;
+	}
+
+	public void setPopulationSize(int populationSize) throws InvalidConfigurationException {
+		this.checkBeforeEditSettings();
+		this.populationSize = populationSize;
+	}
 	
+	public int getPopulationSize() {
+		return populationSize;
+	}	
+
+	public boolean isLocked() {
+		return this.isLocked;
+	}
+
+	public void lockSettings() throws InvalidConfigurationException {
+		this.checkSettings();
+		this.isLocked = true;
+	}
+
+	public void checkSettings() throws InvalidConfigurationException {
+		if(this.breeder == null) {
+			throw new InvalidConfigurationException("There is no breeder");
+		}
+		
+		if(this.chromosomeFactory == null) {
+			throw new InvalidConfigurationException("There is no chromosome factory");
+		}
+		
+		if(this.fitnessFunction == null) {
+			throw new InvalidConfigurationException("There is no fitness function");
+		}
+		
+		if(this.constraints.size() < 1) {
+			throw new InvalidConfigurationException("There is no constraint for determining the end of the processing !");
+		}
+
+		if(this.operators.size() < 1) {
+			throw new InvalidConfigurationException("There is no genetic operator !");
+		}
+		
+		if(this.selectors.size() < 1) {
+			throw new InvalidConfigurationException("There is no selector !");
+		}
+		
+		if(this.populationSize < 1) {
+			throw new InvalidConfigurationException("The size of the population to use is too weak");
+		}
+	}
+	
+	private void checkBeforeEditSettings() throws InvalidConfigurationException {
+		if(this.isLocked) {
+			throw new InvalidConfigurationException("The current configuration is locked !");
+		}
+	}
+
 }
--- group3_genetic/src/main/java/fr/isen/cir56/group3_genetic/Configuration/ConfigurationInterface.java
+++ group3_genetic/src/main/java/fr/isen/cir56/group3_genetic/Configuration/ConfigurationInterface.java
@@ -1,7 +1,12 @@
 package fr.isen.cir56.group3_genetic.Configuration;
 
 import fr.isen.cir56.group3_genetic.AbstractFitnessFunction;
-import fr.isen.cir56.group3_genetic.Population;
+import fr.isen.cir56.group3_genetic.Breeder.BreederInterface;
+import fr.isen.cir56.group3_genetic.Constraint.ConstraintInterface;
+import fr.isen.cir56.group3_genetic.Genotype.ChromosomeFactoryInterface;
+import fr.isen.cir56.group3_genetic.Operator.OperatorInterface;
+import fr.isen.cir56.group3_genetic.Selector.SelectorInterface;
+import java.util.List;
 
 /**
  *
@@ -9,11 +14,52 @@ import fr.isen.cir56.group3_genetic.Population;
  */
 public interface ConfigurationInterface {
 	
-	void setPopulationSize(int populationSize);
+	void addConstraint(ConstraintInterface constraint) throws InvalidConfigurationException;
+	void removeConstraint(ConstraintInterface contraint) throws InvalidConfigurationException;
+	List<ConstraintInterface> getConstraints();
+	
+	void addSelector(SelectorInterface selector) throws InvalidConfigurationException;
+	void removeSelector(SelectorInterface selector) throws InvalidConfigurationException;
+	List<SelectorInterface> getSelectors();
+	
+	void addOperator(OperatorInterface operator) throws InvalidConfigurationException;
+	void removeOperator(OperatorInterface operator) throws InvalidConfigurationException;
+	List<OperatorInterface> getOperators();
+	
+	/**
+	 * Function which will create the best Chromosome
+	 * @param fitnessFunctionInterface
+	 * @throws InvalidConfigurationException 
+	 */
+	void setFitnessFunction(AbstractFitnessFunction fitnessFunctionInterface) throws InvalidConfigurationException;
+	AbstractFitnessFunction getFitnessFunction();
+	
+	/*
+	 * Evolves the population
+	 */
+	void setBreeder(BreederInterface breeder) throws InvalidConfigurationException;
+	BreederInterface getBreeder();
+	
+	/*
+	 * Generate a population at the beginning if the initial population is empty
+	 */
+	void setChromosomeFactory(ChromosomeFactoryInterface chromosomeFactory) throws InvalidConfigurationException;
+	ChromosomeFactoryInterface getChromosomeFactory();
+	
+	/**
+	 * Set the size of the population to use
+	 * @param populationSize 
+	 */
+	void setPopulationSize(int populationSize)  throws InvalidConfigurationException;
 	int getPopulationSize();
 	
-	void setFitnessFunction(AbstractFitnessFunction fitnessFunctionInterface);
-	void setInitialPopulation(Population population);
+	/*
+	 * Locking the settings
+	 * Permit a light constructor by passing parameters with accessors
+	 */
+	boolean isLocked();
+	void lockSettings() throws InvalidConfigurationException;
+	void checkSettings() throws InvalidConfigurationException;
 	
 	
 }
