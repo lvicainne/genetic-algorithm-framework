1842fcbf681c8edf605629e9f32d21ff4d7424d4
Louis VICAINNE <louis@vicainne.fr>
2013-03-21 15:39:39 +0000

    Correction d'implémentationd de Min1DValue (suppression du mot clé final pour x/y)

--- group3_genetic/src/main/java/fr/isen/cir56/group3_genetic/Implementations/min1d/Min1DValue.java
+++ group3_genetic/src/main/java/fr/isen/cir56/group3_genetic/Implementations/min1d/Min1DValue.java
@@ -5,24 +5,15 @@
 package fr.isen.cir56.group3_genetic.Implementations.min1d;
 
 import fr.isen.cir56.group3_genetic.Genotype.AbstractGene;
-import fr.isen.cir56.group3_genetic.Genotype.ChromosomeInterface;
 import fr.isen.cir56.group3_genetic.Genotype.GeneInterface;
-import fr.isen.cir56.group3_genetic.Genotype.InvalidSizeChromosomeCrossoverException;
-import fr.isen.cir56.group3_genetic.Operator.AbstractOperator;
-import fr.isen.cir56.group3_genetic.PopulationInterface;
-import java.awt.Point;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
 
 /**
  *
  * @author Wasp
  */
 public class Min1DValue extends AbstractGene implements GeneInterface {
-
-	private final double x;
-	private final double y;
+	private double x;
+	private double y;
 
 	public Min1DValue(double x, double y) {
 		this.x = x;
