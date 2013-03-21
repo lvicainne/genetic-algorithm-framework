5ac4e176135d0218ba8fba9f3a9ac089620d36d9
Louis VICAINNE <louis@vicainne.fr>
2013-03-21 16:38:29 +0000

    Suppression de classes inutiles Mise à jour de App.java Correction du package importé (au lieu de JGraph, o importe JGraphX plus récent)

--- group3_genetic/src/main/java/fr/isen/cir56/group3_genetic/App.java
+++ group3_genetic/src/main/java/fr/isen/cir56/group3_genetic/App.java
@@ -1,19 +1,25 @@
 package fr.isen.cir56.group3_genetic;
 
+import com.mxgraph.swing.mxGraphComponent;
+import com.mxgraph.util.mxConstants;
+import com.mxgraph.view.mxGraph;
+import com.mxgraph.view.mxStylesheet;
 import fr.isen.cir56.group3_genetic.Configuration.Configuration;
 import fr.isen.cir56.group3_genetic.Configuration.InvalidConfigurationException;
 import fr.isen.cir56.group3_genetic.Constraint.ConstraintInterface;
 import fr.isen.cir56.group3_genetic.Constraint.NumberGenerationConstraint;
 import fr.isen.cir56.group3_genetic.Controller.GeneticController;
 import fr.isen.cir56.group3_genetic.Implementations.tsp.TspChromosomeFactory;
-import fr.isen.cir56.group3_genetic.Operator.OrderedCrossoverOperator;
 import fr.isen.cir56.group3_genetic.Implementations.tsp.TspFitnessFunction;
-import fr.isen.cir56.group3_genetic.Utils.Math.Probability.InvalidProbabilityValueException;
 import fr.isen.cir56.group3_genetic.Model.GeneticModel;
+import fr.isen.cir56.group3_genetic.Operator.OrderedCrossoverOperator;
 import fr.isen.cir56.group3_genetic.Operator.OrderedMutationOperator;
 import fr.isen.cir56.group3_genetic.Selector.RankSelector;
 import fr.isen.cir56.group3_genetic.Selector.SelectorInterface;
+import fr.isen.cir56.group3_genetic.Utils.Math.Probability.InvalidProbabilityValueException;
+import fr.isen.cir56.group3_genetic.View.Configurator.ConfiguratorLauncher;
 import fr.isen.cir56.group3_genetic.View.Graph.AbstractGraphView;
+import fr.isen.cir56.group3_genetic.View.Graph.EvolutionPercentageGraph;
 import fr.isen.cir56.group3_genetic.View.Graph.FitnessEvolutionGraph;
 import fr.isen.cir56.group3_genetic.View.Graph.PopulationSizeGraph;
 import fr.isen.cir56.group3_genetic.View.MainFrameView;
@@ -23,6 +29,7 @@ import fr.isen.cir56.group3_genetic.View.ViewInterface;
 import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.GridLayout;
+import java.util.Hashtable;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JPanel;
@@ -35,27 +42,53 @@ import javax.swing.JPanel;
 public class App {
 
 	public static void main(String[] args) {
-		
+		MainFrameView graphicFrame = new MainFrameView();
 
-		try {
-			org.nfunk.jep.JEP myParser = new org.nfunk.jep.JEP();
+		//DEBUT TEMP
+		mxGraph graph = new mxGraph();
+		Object parent = graph.getDefaultParent();
+
+		graph.getModel().beginUpdate();
+		try
+		{
+			/*Object v1 = graph.insertVertex(parent, null, "Hello", 20, 20, 80,
+					30);
+			Object v2 = graph.insertVertex(parent, null, "World!", 240, 150,
+					80, 30);
+			graph.insertEdge(parent, null, "Edge", v1, v2);
 			
-			myParser.addStandardFunctions();
-			myParser.addStandardConstants();
-			myParser.addVariable("x", 10);
-			myParser.parseExpression("1+x");
+			*/
+            // get stylesheet
+            mxStylesheet stylesheet = graph.getStylesheet();
 
-			double result = myParser.getValue();
-			System.out.println("x + 1 = " + result);
-			myParser.addVariable("x", 12);
-			result = myParser.getValue();
-			System.out.println("x + 1 = " + result);
-		} catch (Exception e) {
-			System.out.println("Error with evaluation");
+            // define stylename
+            String styleName = "myImageStyle";
+
+            // create image style
+            Hashtable<String, Object> style = new Hashtable<>();
+            style.put( mxConstants.STYLE_SHAPE, mxConstants.SHAPE_IMAGE);
+            style.put( mxConstants.STYLE_IMAGE, "/resources/clock.png");
+            style.put( mxConstants.STYLE_VERTICAL_LABEL_POSITION, mxConstants.ALIGN_CENTER);
+
+            // make new style available via stylename
+            stylesheet.putCellStyle( styleName, style);
+
+            // example 1: use style by stylename
+            Object v1 = graph.insertVertex(parent, null, "v1", 100, 100, 80, 30, styleName);
+
+            // example 2: use style by style definition
+            Object v2 = graph.insertVertex(parent, null, "v2", 200, 200, 80, 30, "shape=image;image=/resources/clock.png");
+
+            // create edge
+            graph.insertEdge(parent, null, "", v1, v2);
+		}
+		finally
+		{
+			graph.getModel().endUpdate();
 		}
 
-
-
+		//FIN TEMP
+		
 		Configuration configuration = new Configuration();
 		TspChromosomeFactory chromosomeFactory = new TspChromosomeFactory(configuration);
 		AbstractFitnessFunction fitnessFunction = new TspFitnessFunction(chromosomeFactory);
@@ -76,17 +109,25 @@ public class App {
 		}
 
 		ViewInterface terminalView = new TerminalView(System.err);
-		MainFrameView graphicFrame = new MainFrameView();
 
 		AbstractGraphView graphFitness = new FitnessEvolutionGraph();
 		AbstractGraphView graphPopulationSize = new PopulationSizeGraph();
+		AbstractGraphView graphEvolutionPercentage = new EvolutionPercentageGraph();
 
 		JPanel panel = new JPanel();
 		panel.setLayout(new GridLayout(2, 2));
 		panel.add(graphFitness.getJPanel());
 		panel.add(graphPopulationSize.getJPanel());
-		panel.add(graphPopulationSize.getJPanel());
-		panel.add(graphPopulationSize.getJPanel());
+		panel.add(graphEvolutionPercentage.getJPanel());
+
+		//DEBUT TEMP
+		mxGraphComponent graphComponent = new mxGraphComponent(graph);
+		panel.add(graphComponent);
+
+		JPanel configurateurLauncher = new ConfiguratorLauncher();
+		panel.add(configurateurLauncher);
+		//FIN TEMP
+		
 		graphicFrame.add(panel);
 
 		GeneticModel model = new GeneticModel(configuration);
@@ -101,6 +142,7 @@ public class App {
 		model.addView(toolbar);
 		model.addView(graphFitness);
 		model.addView(graphPopulationSize);
+		model.addView(graphEvolutionPercentage);
 
 	}
 }
