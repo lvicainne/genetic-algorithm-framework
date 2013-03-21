43198706e6774ccea1fab73d7f0bdf06732c32f4
Louis VICAINNE <louis@vicainne.fr>
2013-03-21 14:31:27 +0000

    Ajout de lib externe de reflection pour la recherche des classes parentes.

---
+++ group3_genetic/src/main/java/fr/isen/cir56/group3_genetic/Utils/Reflection/Generalization.java
@@ -0,0 +1,46 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.isen.cir56.group3_genetic.Utils.Reflection;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ *
+ *
+ * Copyright 2003 Sapient
+ * @since carbon 2.0
+ * @author Greg Hinkle, March 2003
+ * @version $Revision: 1.5 $($Author: dvoet $ / $Date: 2003/05/05 21:21:23 $)
+ */
+public class Generalization {
+	/**
+     * Builds an <b>unordered</b> set of all interface and object classes that
+     * are generalizations of the provided class.
+     * @param classObject the class to find generalization of.
+     * @return a Set of class objects.
+     */
+    public static Set<Class> getGeneralizations(Class classObject) {
+        Set<Class> generalizations = new HashSet<>();
+
+        generalizations.add(classObject);
+
+        Class superClass = classObject.getSuperclass();
+        if (superClass != null) {
+            generalizations.addAll(getGeneralizations(superClass));
+        }
+
+        Class[] superInterfaces = classObject.getInterfaces();
+        for (int i = 0; i < superInterfaces.length; i++) {
+            Class superInterface = superInterfaces[i];
+            generalizations.addAll(getGeneralizations(superInterface));
+        }
+
+        return generalizations;
+    }
+}
